a288f62082b78ee4d9c92626bfa6d9d03ff60372
채지원 <dev.julia.chae@gmail.com>
2025-04-02 15:28:34 +0900

    design: Button 공용 컴포넌트 추가

---
+++ src/components/Button.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button as MuiButton } from "@mui/material";
+
+interface ButtonType {
+  text: string;
+  className?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
+  rightElement?: React.ReactNode;
+  size?: "small" | "medium" | "large";
+  onMouseEnter?: React.MouseEventHandler<HTMLButtonElement>;
+  onMouseLeave?: React.MouseEventHandler<HTMLButtonElement>;
+  id?: string;
+  sx?: object;
+}
+
+const Button = ({
+  text,
+  className,
+  onClick,
+  disabled = false,
+  onMouseEnter,
+  onMouseLeave,
+  id,
+  ...props
+}: ButtonType) => {
+  return (
+    <MuiButton
+      id={id}
+      disabled={disabled}
+      className={className}
+      onClick={onClick}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      {...props}
+    >
+      <span>{text}</span>
+    </MuiButton>
+  );
+};
+
+export default Button;

59c896ce2e537b9764ea088207a26e66e7d45c09
채지원 <dev.julia.chae@gmail.com>
2025-04-03 12:25:03 +0900

    feat: 로그인 이후 접근 가능 페이지에 상단바 보이도록 코드 추가

--- src/components/layout/PrivateRoute.tsx
+++ src/components/layout/PrivateRoute.tsx
@@ -1,15 +1,44 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Outlet, Navigate } from "react-router-dom";
+import NavigationBar from "@components/NavigationBar";
 
 const PrivateRoute = () => {
   const isSignedIn = Boolean(sessionStorage.getItem("_ZA"));
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    if (userInfo == null) {
+      axios
+        .get(`${import.meta.env.VITE_SERVER_URL}/user/me`, {
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("_ZA") || ""}`,
+            "Content-Type": "application/json",
+          },
+        })
+        .then((res) => {
+          const userData = res?.data?.data;
+          if (res.status === 200 && userData) {
+            setUserInfo(userData);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [userInfo]);
 
   if (!isSignedIn) {
     localStorage.clear();
     return <Navigate to="/sign-in" />;
   }
 
+  if (!userInfo) return <div>loading ...</div>;
+
   return (
     <>
+      <NavigationBar userInfo={userInfo} />
       <div className="flex-1">
         <Outlet />
       </div>

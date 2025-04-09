90f684da24b05e5c8211e55a94fcefaa06deb6fb
채지원 <dev.julia.chae@gmail.com>
2025-04-10 07:50:16 +0900

    chore: lint 에러 수정

--- src/components/layout/PrivateRoute.tsx
+++ src/components/layout/PrivateRoute.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import NavigationBar from "@components/NavigationBar";
 import apiClient from "@apis/apiClient";

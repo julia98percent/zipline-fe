5b4564069c0afacb7a17ef3fc29fa025d602099d
채지원 <dev.julia.chae@gmail.com>
2025-05-13 12:00:52 +0900

    feat: 고객/매물 벌크 업로드 기능 추가

--- src/pages/PrivatePropertyListPage/PropertyAddButtonList/PropertyAddButtonList.tsx
+++ src/pages/PrivatePropertyListPage/PropertyAddButtonList/PropertyAddButtonList.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Button from "@components/Button";
 import PropertyAddModal from "./PropertyAddModal";
+import BulkUploadModal from "./BulkUploadModal";
 import AddIcon from "@mui/icons-material/Add";
 import { Button as MuiButton } from "@mui/material";
 
@@ -9,40 +10,44 @@ interface Props {
 }
 
 function PropertyAddButtonList({ fetchPropertyData }: Props) {
-  const [open, setOpen] = useState(false); // 모달 열림 상태 관리
+  const [open, setOpen] = useState(false);
+  const [openBulkUpload, setOpenBulkUpload] = useState(false);
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const handleOpenBulkUpload = () => setOpenBulkUpload(true);
+  const handleCloseBulkUpload = () => setOpenBulkUpload(false);
 
   return (
     <>
       <div>
-      <MuiButton
-              variant="contained"
-              startIcon={<AddIcon />}
-              onClick= {handleOpen}
-              sx={{
-                backgroundColor: "#164F9E",
-                "&:hover": { backgroundColor: "#0D3B7A" },
-                height: "36px",
-                fontSize: "13px",
-                padding: "0 16px",
-              }}
-            >
-              매물 등록
-            </MuiButton>
+        <MuiButton
+          variant="contained"
+          startIcon={<AddIcon />}
+          onClick={handleOpen}
+          sx={{
+            backgroundColor: "#164F9E",
+            "&:hover": { backgroundColor: "#0D3B7A" },
+            height: "36px",
+            fontSize: "13px",
+            padding: "0 16px",
+          }}
+        >
+          매물 등록
+        </MuiButton>
 
         <Button
           text="매물 데이터 일괄 등록(.csv)"
-          disabled
+          variant="outlined"
+          onClick={handleOpenBulkUpload}
           sx={{
-            color: "white",
+            color: "#164F9E",
             minHeight: "32px",
-            backgroundColor: "#164F9E",
+            borderColor: "#164F9E",
             marginLeft: "12px",
-            "&:disabled": {
-              backgroundColor: "lightgray",
-              color: "white",
+            "&:hover": {
+              borderColor: "#0D3B7A",
+              color: "#0D3B7A",
             },
           }}
         />
@@ -50,6 +55,11 @@ function PropertyAddButtonList({ fetchPropertyData }: Props) {
       <PropertyAddModal
         open={open}
         handleClose={handleClose}
+        fetchPropertyData={fetchPropertyData}
+      />
+      <BulkUploadModal
+        open={openBulkUpload}
+        handleClose={handleCloseBulkUpload}
         fetchPropertyData={fetchPropertyData}
       />
     </>

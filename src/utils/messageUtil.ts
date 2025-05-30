05c0e1144a82e443950e2941948f8820fbc8d901
채지원 <dev.julia.chae@gmail.com>
2025-05-30 18:35:51 +0900

    Feat: 문자 관련 util 추가

---
+++ src/utils/messageUtil.ts
@@ -0,0 +1,28 @@
+import { MSG_ERROR_MESSAGES, MESSAGE_STATUS } from "@constants/Message";
+
+export const getStatusMessage = (statusCode: string): string => {
+  if (statusCode.startsWith("4")) {
+    return MESSAGE_STATUS.SENT;
+  } else if (statusCode.startsWith("2")) {
+    return MESSAGE_STATUS.RECEIVED;
+  } else if (statusCode.startsWith("3")) {
+    return MESSAGE_STATUS.CARRIER_RECEIVED;
+  } else {
+    return MESSAGE_STATUS.FAILED;
+  }
+};
+
+export const getErrorMessage = (statusCode: string): string => {
+  return MSG_ERROR_MESSAGES[statusCode] || "알 수 없는 오류입니다.";
+};
+
+export const translateMessageStatusToKorean = (status: string) => {
+  switch (status) {
+    case "COMPLETE":
+      return "접수 완료";
+    case "FAILED":
+      return "접수 실패";
+    default:
+      return "접수 대기";
+  }
+};

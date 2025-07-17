4319270ae82222f319290bcd683ebd7e8399bdfa
채지원 <dev.julia.chae@gmail.com>
2025-07-17 17:50:04 +0900

    Chore: mui 컴포넌트 내에서 className으로 tailwindcss 문법 사용할 수 있도록 설정 코드 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { StyledEngineProvider } from "@mui/material/styles";
+import GlobalStyles from "@mui/material/GlobalStyles";
 import App from "./App.tsx";
 import "./main.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StyledEngineProvider enableCssLayer>
+    <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StyledEngineProvider>
 );

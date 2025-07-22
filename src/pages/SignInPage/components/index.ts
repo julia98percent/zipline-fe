a4e2bd1ffb64f489d79c1a8c60036844c7acb4be
채지원 <dev.julia.chae@gmail.com>
2025-07-23 00:07:55 +0900

    Design: 기존 로그인 페이지에 회원 정보 찾기 페이지 연결 링크 추가

--- src/pages/SignInPage/components/index.ts
+++ src/pages/SignInPage/components/index.ts
@@ -1,3 +1,4 @@
 export { default as UserIdInput } from "./UserIdInput";
 export { default as PasswordInput } from "./PasswordInput";
 export { default as Header } from "./Header";
+export { default as AuthLinks } from "./AuthLinks";

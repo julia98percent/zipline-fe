29a5040ea18bbcc89ea5556c5930a955ea04009b
채지원 <dev.julia.chae@gmail.com>
2025-07-29 18:06:45 +0900

    Fix: 알림 영역 잘려보이는 버그 수정

--- src/components/PageHeader/NotificationList/NotificationList.tsx
+++ src/components/PageHeader/NotificationList/NotificationList.tsx
@@ -46,7 +46,7 @@ function NotificationList({
 
   return (
     <>
-      <Box className="absolute top-15 -right-12 bg-white shadow-lg rounded-lg p-4 z-50 w-2/5 min-w-75 max-w-96 max-h-1/2 overflow-hidden flex flex-col">
+      <Box className="absolute top-15 -right-12 bg-white shadow-lg rounded-lg p-4 z-50 w-2/5 min-w-75 max-w-96 max-h-[50vh] overflow-hidden flex flex-col">
         <div className="flex items-center justify-between text-gray-900 font-semibold mb-3 pb-2 border-b border-gray-200">
           <Typography variant="h6">알림</Typography>
           <Button

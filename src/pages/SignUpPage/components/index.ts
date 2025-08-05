c279c14c6cdbaa94988ddab4179c9a6a1147bc8b
채지원 <dev.julia.chae@gmail.com>
2025-08-05 17:42:08 +0900

    Fix: 빌드 깨지는 부분 수정

--- src/pages/SignUpPage/components/index.ts
+++ src/pages/SignUpPage/components/index.ts
@@ -4,4 +4,3 @@ export { default as Header } from "./Header";
 export { default as NameInput } from "./NameInput";
 export { default as EmailInput } from "./EmailInput";
 export { default as PhoneNumberInput } from "./PhoneNumberInput";
-export { default as PasswordCheckInput } from "./PasswordCheckInput";

591e4fa32208ed02002f62e86145a417eb074aa8
채지원 <dev.julia.chae@gmail.com>
2025-08-05 17:40:09 +0900

    Feat: 캡스락 알림 컴포넌트를 비밀번호 관련 인풋 파일에 추가

--- src/pages/SignInPage/components/PasswordInput.tsx
+++ src/pages/SignInPage/components/PasswordInput.tsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from "react";
 import TextField from "@components/TextField";
+import CapsLockWarning from "@components/CapsLockWarning";
 
 interface Props {
   password: string;
@@ -11,14 +13,43 @@ const PasswordInput = ({
   handleChangePassword,
   onKeyDown,
 }: Props) => {
+  const [showCapsLockWarning, setShowCapsLockWarning] = useState(false);
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const isCapsLockOn = e.getModifierState("CapsLock");
+    const isEnglishLetter = /^[a-zA-Z]$/.test(e.key);
+
+    if (isCapsLockOn && isEnglishLetter && !showCapsLockWarning) {
+      setShowCapsLockWarning(true);
+    }
+
+    if (onKeyDown) {
+      onKeyDown(e);
+    }
+  };
+
+  useEffect(() => {
+    if (showCapsLockWarning) {
+      const timer = setTimeout(() => {
+        setShowCapsLockWarning(false);
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [showCapsLockWarning]);
+
   return (
-    <TextField
-      label="비밀번호"
-      type="password"
-      value={password}
-      onChange={handleChangePassword}
-      onKeyDown={onKeyDown}
-    />
+    <div style={{ position: "relative" }}>
+      <CapsLockWarning show={showCapsLockWarning} />
+      <TextField
+        label="비밀번호"
+        type="password"
+        value={password}
+        onChange={handleChangePassword}
+        onKeyDown={handleKeyPress}
+        fullWidth
+      />
+    </div>
   );
 };
 

6fe74364346d6580bff5a85aa32a6c2d4980e6b4
채지원 <dev.julia.chae@gmail.com>
2025-09-09 17:01:44 +0900

    Refactor: tailwindcss 사용을 위한 postcss.config.js 파일 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-		"./index.html",
-		"./src/**/*.{js,ts,jsx,tsx}",
-		"./node_modules/flowbite/**/*.js",
-        ],
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {},
   },

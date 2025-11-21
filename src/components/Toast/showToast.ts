30a192adde0bf166886a9db2da7da70ee299985c
채지원 <dev.julia.chae@gmail.com>
2025-11-21 15:46:06 +0900

    fix: 토스트 중복으로 뜨는 문제 수정

--- src/components/Toast/showToast.ts
+++ src/components/Toast/showToast.ts
@@ -8,11 +8,18 @@ interface ToastProps {
 }
 
 const showToast = ({ message, type, duration = 2000 }: ToastProps) => {
+  const toastId = `${type}-${message}`;
+
+  if (toast.isActive(toastId)) {
+    return;
+  }
+
   toast[type](message, {
+    toastId,
     autoClose: duration,
     draggable: true,
     className:
-      "w-[70vw]! xs:w-[60vw]! sm:w-[40vw]! md:w-[30vw]! lg:w-[22vw]! mb-2! mr-[20px]",
+      "w-[70vw]! xs:w-[60vw]! sm:w-[40vw]! md:w-[30vw]! lg:w-[22vw]! mb-2! mt-[20px] mr-[20px]",
   });
 };
 

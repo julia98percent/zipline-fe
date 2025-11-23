074c5299fbbab440a8e076c967befeb65431a56a
채지원 <dev.julia.chae@gmail.com>
2025-11-23 18:09:09 +0900

    fix: 로그인 성공 했어도 대시보드 화면으로 리다이렉트 되지 않는 버그 수정

--- src/components/layout/GuestRoute.tsx
+++ src/components/layout/GuestRoute.tsx
@@ -2,16 +2,31 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import useAuthStore from "@/stores/useAuthStore";
+import CircularProgress from "@/components/CircularProgress";
 
 const GuestRoute = ({ children }: { children: React.ReactNode }) => {
-  const { isSignedIn } = useAuthStore();
+  const { isSignedIn, checkAuth } = useAuthStore();
   const router = useRouter();
+
+  useEffect(() => {
+    if (isSignedIn === null) {
+      checkAuth();
+    }
+  }, [isSignedIn, checkAuth]);
 
   useEffect(() => {
     if (isSignedIn === true) {
       router.replace("/");
     }
   }, [isSignedIn, router]);
+
+  if (isSignedIn === null) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <CircularProgress />
+      </div>
+    );
+  }
 
   if (isSignedIn === true) {
     return null;
